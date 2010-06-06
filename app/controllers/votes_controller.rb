809da8ab91527fccbd6611286383ecae759e98ba
Paul Elliott <paul@hashrocket.com>
2010-06-05 22:58:17 -0400

    Switch to use increment_counter

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -3,11 +3,10 @@ class VotesController < ApplicationController
   expose(:poll) { Poll.find_by_id(params[:poll_id]) }
 
   def create
-    if params[:choice] == 'bohemian'
-      poll.update_attribute(:bohemian, poll.bohemian + 1)
-    else
-      poll.update_attribute(:stairway, poll.stairway + 1)
-    end
+    Poll.increment_counter(
+      params[:choice] == 'bohemian' ? :bohemian : :stairway,
+      params[:poll_id]
+    )
     redirect_to poll_path(poll)
   end
 
